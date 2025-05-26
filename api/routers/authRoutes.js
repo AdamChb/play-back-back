f001a1144392db09f49320bb8b04f0b177dc8208
Sacha Portal <sacha.portal@efrei.net>
2025-05-26 23:41:16 +0200

    Ajout recherche user par string

--- api/routers/authRoutes.js
+++ api/routers/authRoutes.js
@@ -1,15 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authController = require('../controllers/authControllers');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const authController = require("../controllers/authControllers");
+const { authenticateToken } = require("../middleware/authMiddleware");
 
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-router.post('/create', authenticateToken, authController.createUser);
-router.post('/update', authenticateToken, authController.updateUser);
-router.delete('/delete', authenticateToken, authController.deleteUser);
-router.get('/getUser', authenticateToken, authController.getUser);
-router.get('/getAllUsers', authenticateToken, authController.getAllUsers);
+router.post("/login", authController.login);
+router.post("/register", authController.register);
+router.post("/create", authenticateToken, authController.createUser);
+router.post("/update", authenticateToken, authController.updateUser);
+router.delete("/delete", authenticateToken, authController.deleteUser);
+router.get("/getUser", authenticateToken, authController.getUser);
+router.get("/getAllUsers", authenticateToken, authController.getAllUsers);
+router.get("/search", authenticateToken, authController.searchUsers);
 
 module.exports = router;
